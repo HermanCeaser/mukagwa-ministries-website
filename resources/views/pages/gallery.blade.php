6395d68973b9d03091f4f5baade08bc4281e0f1d
Henry Ssegwanyi <henry.ssegwanyi@efficiencie.com>
2023-04-04 15:01:52 +0300

    Uploading Photos

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -7,7 +7,7 @@
                     <div class="commn_banner_page">
                         <h2><span class="color_big_heading">Our</span>Gallery</h2>
                         <ul class="breadcrumb_wrapper">
-                            <li class="breadcrumb_item"><a href="{{route('home')}}">Home</a></li>
+                            <li class="breadcrumb_item"><a href="{{ route('home') }}">Home</a></li>
                             <li class="breadcrumb_item"><img src="assets/img/icon/arrow.png" alt="img"></li>
                             <li class="breadcrumb_item active">Gallery Slider</li>
                         </ul>
@@ -30,21 +30,105 @@
                 </div>
             </div>
             <div class="row popup-gallery">
-                <div class="col-lg-12 co-md-12 col-sm-12 col-12">
-                    <div class="gallery_slider_area owl-theme owl-carousel slider_dots">
-                        @for ($i=1; $i<18; $i++)
+                {{-- <div class="col-lg-12 co-md-12 col-sm-12 col-12"> --}}
+                {{-- <div class="gallery_slider_area owl-theme owl-carousel slider_dots"> --}}
+                @for ($i = 1; $i < 18; $i++)
+                    <div class="col-lg-4 co-md-6 col-sm-12 col-12">
                         <div class="gallery_item">
-                            <img src="{{asset('assets/img/gallery/gallery' . $i  . '.jpg')}}" alt="gallery images">
+                            <img src="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" alt="gallery images">
                             <div class="gallery_overlay">
-                                <a href="{{asset('assets/img/gallery/gallery' . $i  . '.jpg')}}" title="FoodCamp"><img
+                                <a href="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" title="FoodCamp"><img
                                         src="assets/img/icon/arrow-round.png" alt="icon"></a>
                             </div>
                         </div>
-                        @endfor
-
-
                     </div>
-                </div>
+                @endfor
+                {{-- </div> --}}
+                {{-- </div> --}}
+
+            </div>
+            <h2> Meeting Pastors <span class="color_big_heading">in </span> Luwero-District
+            </h2>
+            <div class="row popup-gallery">
+                {{-- <div class="col-lg-12 co-md-12 col-sm-12 col-12"> --}}
+                {{-- <div class="gallery_slider_area owl-theme owl-carousel slider_dots"> --}}
+                @for ($i = 18; $i < 20; $i++)
+                    <div class="col-lg-4 co-md-6 col-sm-12 col-12">
+                        <div class="gallery_item">
+                            <img src="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" alt="gallery images">
+                            <div class="gallery_overlay">
+                                <a href="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" title="FoodCamp"><img
+                                        src="assets/img/icon/arrow-round.png" alt="icon"></a>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+                {{-- </div> --}}
+                {{-- </div> --}}
+
+            </div>
+
+            <h2> Advocating & Explaining our Projects <span class="color_big_heading">to</span> Women in Masuliita (Wakiso-District)
+            </h2>
+            <div class="row popup-gallery">
+                {{-- <div class="col-lg-12 co-md-12 col-sm-12 col-12"> --}}
+                {{-- <div class="gallery_slider_area owl-theme owl-carousel slider_dots"> --}}
+                @for ($i = 20; $i < 33; $i++)
+                    <div class="col-lg-4 co-md-6 col-sm-12 col-12">
+                        <div class="gallery_item">
+                            <img src="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" alt="gallery images">
+                            <div class="gallery_overlay">
+                                <a href="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" title="FoodCamp"><img
+                                        src="assets/img/icon/arrow-round.png" alt="icon"></a>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+                {{-- </div> --}}
+                {{-- </div> --}}
+
+            </div>
+
+            <h2> Children's Orphanage <span class="color_big_heading">in</span> in Namutumba-District
+            </h2>
+            <div class="row popup-gallery">
+                {{-- <div class="col-lg-12 co-md-12 col-sm-12 col-12"> --}}
+                {{-- <div class="gallery_slider_area owl-theme owl-carousel slider_dots"> --}}
+                @for ($i = 33; $i < 36; $i++)
+                    <div class="col-lg-4 co-md-6 col-sm-12 col-12">
+                        <div class="gallery_item">
+                            <img src="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" alt="gallery images">
+                            <div class="gallery_overlay">
+                                <a href="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" title="FoodCamp"><img
+                                        src="assets/img/icon/arrow-round.png" alt="icon"></a>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+                {{-- </div> --}}
+                {{-- </div> --}}
+
+            </div>
+
+            <h2> Women's Conference <span class="color_big_heading">in</span> Kampala
+            </h2>
+            <div class="row popup-gallery">
+                {{-- <div class="col-lg-12 co-md-12 col-sm-12 col-12"> --}}
+                {{-- <div class="gallery_slider_area owl-theme owl-carousel slider_dots"> --}}
+                @for ($i = 36; $i < 37; $i++)
+                    <div class="col-lg-4 co-md-6 col-sm-12 col-12">
+                        <div class="gallery_item">
+                            <img src="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" alt="gallery images">
+                            <div class="gallery_overlay">
+                                <a href="{{ asset('assets/img/gallery/gallery' . $i . '.jpg') }}" title="FoodCamp"><img
+                                        src="assets/img/icon/arrow-round.png" alt="icon"></a>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+                {{-- </div> --}}
+                {{-- </div> --}}
+
             </div>
         </div>
     </section>
@@ -52,6 +136,5 @@
 </x-app-layout>
 
 @push('js')
-<script src="{{ asset('assets/js/gallery-popup.js')}}"></script>
+    <script src="{{ asset('assets/js/gallery-popup.js') }}"></script>
 @endpush
-
