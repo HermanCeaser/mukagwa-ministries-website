755fe78b92935792c5fa2baeff638994cb6db7d2
Herman Ceaser <ceaserbanks09@gmail.com>
2023-04-14 02:09:22 +0300

    can send mails

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -26,13 +28,33 @@ class ContactController extends Controller
             'message' => 'required|min:10|max:250'
         ]);
 
+        // $name = $request->name;
+        // $email = $request->email;
+        // $subject = $request->subject;
+        // $message = $request->name;
 
-        Contact::create($request->all());
+        // $EmailTo = "info@mukagwaministries.com";
+        // // Prepare Email Body Text
+        // $Body = null;
+        // $Body .= "<p><b>Name:</b> {$name}</p>";
+        // $Body .= "<p><b>Email:</b> {$email}</p>";
+        // $Body .= "<p><b>Subject:</b> {$subject}</p>";
+        // $Body .= "<p><b>Message:</b> </p><p>{$message}</p>";
 
-        return redirect()->back()->with(['success' => 'Thank you for contacting us. We will get back to you Shortly']);
+        // // Send Email
+        // $headers = 'MIME-Version: 1.0' . "\r\n";
+        // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        // $headers .= 'From:  ' . $name . ' <' . $email . '>' . " \r\n" .
+        //     'Reply-To: ' .  $EmailTo . "\r\n" .
+        //     'X-Mailer: PHP/' . phpversion();
 
-
+        try {
+            // mail($EmailTo, $subject, $message, $headers);
+            $contact = Contact::create($request->all());
+            Mail::to(env('MAIL_FROM_ADDRESS'))->send(new ContactMail($contact));
+            return redirect()->back()->with(['success' => 'Thank you for contacting us. We will get back to you Shortly']);
+        } catch (\Exception $e) {
+            return redirect()->back()->with(['error' => 'Failed to send the Email. Try Again Later']);
+        }
     }
-
-
 }
