c51f8ada3afe5fa1a56e7b039554e3fed3ed0e76
Henry Ssegwanyi <h.ssegwanyi@gmail.com>
2023-04-19 17:07:31 +0300

    Project Manager

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -115,7 +115,7 @@
 
                         <div class="volunteer_text">
                             <h3><a href="#!">Betty Mutunda</a></h3>
-                            <p>Project manager</p>
+                            <p>Project Manager</p>
                         </div>
                     </div>
                 </div>
